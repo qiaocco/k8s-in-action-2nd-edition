d9a6c3d195bb76a05b939f2a61ba07e47064f4d8
wuxia64 <wuxia64@gmail.com>
2021-04-14 14:49:43 +0800

    增加app.js

--- chap5/kubia-stdin-image/app.js
+++ chap5/kubia-stdin-image/app.js
@@ -1,38 +1,38 @@
-const http = require("http");
-const os = require("os");
-const readline = require("readline");
+const http = require('http');
+const os = require('os');
+const readline = require('readline');
 
 const listenPort = 8080;
 
-var greeting = "你好";
+var greeting = "Hey there";
 
-console.log("node服务启动中...");
+console.log("Kubia server starting...");
 console.log("Local hostname is " + os.hostname());
-console.log("服务运行端口是： " + listenPort);
+console.log("Listening on port " + listenPort);
 
 let handler = function (request, response) {
   let clientIP = request.connection.remoteAddress;
-  console.log("收到请求啦, 请求url=" + request.url + ", clientIP=" + clientIP);
-  response.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
-  response.write(greeting + ", 我的hostname是" + os.hostname() + '. ');
-  response.write("你的ip是" + clientIP + '. ');
+  console.log("Received request for " + request.url + " from " + clientIP);
+  response.writeHead(200);
+  response.write(greeting + ", this is " + os.hostname() + ". ");
+  response.write("Your IP is " + clientIP + ". ");
   response.end("\n");
-}
+};
 
-let server = http.createServer(handler)
-server.listen(listenPort)
+let server = http.createServer(handler);
+server.listen(listenPort);
 
 let rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false
-})
+});
 
-rl.on("line", function (line) {
+rl.on('line', function (line) {
   if (line.trim() === "") {
-    console.log("请输入新的欢迎语，并按回车键")
+    console.error("Enter new greeting and press ENTER.");
   } else {
     greeting = line;
-    console.log("欢迎语是：" + line)
+    console.log("Greeting set to: " + line);
   }
-})
+});
